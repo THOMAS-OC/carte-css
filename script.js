18a6445336844181a04b5e846ac5460facd4ac9f
= <=>
2022-01-03 14:26:33 +0000

    Modification du style avec une image quadrillée

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ card.addEventListener("mouseover", () => {
     document.querySelector(".square_right").style.transform = "translate(20px, 20px)";
     document.querySelector(".circle_animation").style.border = "2px solid transparent";
     document.querySelector(".circle_animation2").style.border = "2px solid transparent";
+    document.querySelector(".carte_invisible").style.transform = "translate(-50%, -50%) scale(1.05) rotate(2deg)";
+
 })
 
 card.addEventListener("mouseout", () => {
@@ -14,5 +16,5 @@ card.addEventListener("mouseout", () => {
     document.querySelector(".square_right").style.transform = "translate(-20px, -20px)";
     document.querySelector(".circle_animation").style.border = "2px solid white";
     document.querySelector(".circle_animation2").style.border = "2px solid white";
-
+    document.querySelector(".carte_invisible").style.transform = "translate(-50%, -50%) scale(1) rotate(0deg)";
 })
